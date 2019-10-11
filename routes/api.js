c68043a245331b6e48d0711541151554eab5a466
Kat <littlepawcoding@gmail.com>
2019-10-11 12:44:20 +0200

    error testing route through postman - 404 no default engine was specified

--- routes/api.js
+++ routes/api.js
@@ -5,9 +5,21 @@ const db = require("../model/helper");
 
 router.use(bodyParser.json());
 
+function sendItems(req, res) {
+  // Send back the full list of items
+  db("SELECT * FROM classes ORDER BY id ASC;").then(results => {
+    if (results.error) {
+      // if there is an error
+      // return an error
+      res.status(500).send(results.error);
+    } // else send the data
+    res.send(results.data);
+  });
+}
+
 // get a list of classID
 router.get("/api/classes", (req, res) => {
-  res.send("List of classes");
+  res.send(results.data);
 });
 
 module.exports = router;
