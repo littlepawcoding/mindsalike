1a10c82f6d2903780ea2ecf922491456ff9b8db7
Kat <littlepawcoding@gmail.com>
2019-10-11 21:09:59 +0200

    redownloaded yarn packages and got first endpoint working!

--- routes/api.js
+++ routes/api.js
@@ -5,16 +5,10 @@ const db = require("../model/helper");
 
 router.use(bodyParser.json());
 
-router.get("/api", (req, res) => {
-  res.send("Welcome to the API");
-});
-
 // get a list of classID
 router.get("/classes", (req, res) => {
-  db("SELECT * FROM classes ORDER BY dogID ASC;").then(results => {
+  db("SELECT * FROM classes;").then(results => {
     if (results.error) {
-      // if there is an error
-      // return an error
       res.status(500).send(results.error);
     } //
     res.send(results.data);
