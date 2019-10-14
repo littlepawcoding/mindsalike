474d954a9da34e8bc7a787a394cf7f9310ee120a
Kat <littlepawcoding@gmail.com>
2019-10-14 14:58:07 +0200

    endpoint 6 working!

--- routes/api.js
+++ routes/api.js
@@ -28,10 +28,10 @@ router.get("/classes/:classID", (req, res) => {
   });
 });
 
-// ENDPOINT 3) give me a list of the classes dogID 1 is taking
-// localhost:5000/dog_classes/dogID/classID;
-router.get("/dog_classes/:dogID/:classID", (req, res) => {
-  db("SELECT * FROM dog_classes WHERE dogID=1 ORDER BY classID;").then(
+// ENDPOINT 3) give me a list of the dogs taking classID 9
+// localhost:5000/dog_classes/classID/dogID;
+router.get("/dog_classes/:classID/:dogID", (req, res) => {
+  db("SELECT * FROM dog_classes WHERE classID=9 ORDER BY dogID;").then(
     results => {
       if (results.error) {
         res.status(500).send(results.error);
@@ -52,7 +52,7 @@ router.get("/dogs", (req, res) => {
   });
 });
 
-// ENDPOINT 5) give me a specific class with a certain id
+// ENDPOINT 5) give me a specific dog with a certain id
 // localhost:5000/dogs/dogID;
 router.get("/dogs/:dogID;", (req, res) => {
   db("SELECT * FROM dogs WHERE dogID=5;").then(results => {
@@ -63,11 +63,23 @@ router.get("/dogs/:dogID;", (req, res) => {
   });
 });
 
+// ENDPOINT 6) give me a list of the classes dogID 1 is taking
+// localhost:5000/dog_classes/dogID/classID;
+router.get("/dog_classes/:dogID/:classID", (req, res) => {
+  db("SELECT * FROM dog_classes WHERE dogID=1 ORDER BY classID;").then(
+    results => {
+      if (results.error) {
+        res.status(500).send(results.error);
+      } //
+      res.send(results.data);
+    }
+  );
+});
+
 // Endpoints:
 // SELECT * FROM classes;
 // SELECT * FROM classes WHERE classID=7;
-// SELECT * FROM dog_classes WHERE dogID=1 ORDER BY classID;
-// SELECT * FROM dog_classes WHERE classID=3 ORDER BY dogID;
+// SELECT * FROM dog_classes WHERE classID=9 ORDER BY dogID;
 // SELECT * FROM dogs;
 // SELECT * FROM dogs WHERE dogID=5;
 // SELECT * FROM dog_classes WHERE dogID=1 ORDER BY classID;
