92112cc93f74ffd0ed36ea82837216fbd8117ec7
Kat <littlepawcoding@gmail.com>
2019-10-14 17:09:46 +0200

    tidied up endpoint code - now able to choose an ID rather than it being hard coded

--- routes/api.js
+++ routes/api.js
@@ -18,20 +18,21 @@ router.get("/classes", (req, res) => {
 
 // ENDPOINT 2) give me a specific class with a certain id
 // localhost:5000/classes/classID
-// how can I get this so I can choose which class to view?
 router.get("/classes/:classID", (req, res) => {
-  db("SELECT * FROM classes WHERE classID=7;").then(results => {
-    if (results.error) {
-      res.status(500).send(results.error);
-    } //
-    res.send(results.data);
-  });
+  db(`SELECT * FROM classes WHERE classID=${req.params.classID};`).then(
+    results => {
+      if (results.error) {
+        res.status(500).send(results.error);
+      } //
+      res.send(results.data);
+    }
+  );
 });
 
 // ENDPOINT 3) give me a list of the dogs taking classID 9
-// localhost:5000/dog_classes/classID/dogID;
-router.get("/dog_classes/:classID/:dogID", (req, res) => {
-  db("SELECT * FROM dog_classes WHERE classID=9 ORDER BY dogID;").then(
+// localhost:5000/dog_classes/9;
+router.get("/dog_classes/:classID", (req, res) => {
+  db(`SELECT * FROM dog_classes WHERE classID=${req.params.classID};`).then(
     results => {
       if (results.error) {
         res.status(500).send(results.error);
@@ -53,9 +54,9 @@ router.get("/dogs", (req, res) => {
 });
 
 // ENDPOINT 5) give me a specific dog with a certain id
-// localhost:5000/dogs/dogID;
+// localhost:5000/dogs/3;
 router.get("/dogs/:dogID;", (req, res) => {
-  db("SELECT * FROM dogs WHERE dogID=5;").then(results => {
+  db(`SELECT * FROM dogs WHERE dogID=${req.params.dogID};`).then(results => {
     if (results.error) {
       res.status(500).send(results.error);
     } //
@@ -64,9 +65,9 @@ router.get("/dogs/:dogID;", (req, res) => {
 });
 
 // ENDPOINT 6) give me a list of the classes dogID 1 is taking
-// localhost:5000/dog_classes/dogID/classID;
-router.get("/dog_classes/:dogID/:classID", (req, res) => {
-  db("SELECT * FROM dog_classes WHERE dogID=1 ORDER BY classID;").then(
+// localhost:5000/dog_classes/1;
+router.get("/dog_classes/:dogID", (req, res) => {
+  db(`SELECT * FROM dog_classes WHERE dogID=${req.params.dogID};`).then(
     results => {
       if (results.error) {
         res.status(500).send(results.error);
